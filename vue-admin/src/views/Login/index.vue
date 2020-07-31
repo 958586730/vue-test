cfda607ae7a2e96f45804fc92a08808e5c0bd3ec
chenxinchao <958586730@qq.com>
2020-07-31 18:57:07 +0800

    1

--- vue-admin/src/views/Login/index.vue
+++ vue-admin/src/views/Login/index.vue
@@ -1,11 +1,17 @@
 <template>
-    <div>22222222222222222222222222222222222222222</div>
+  <el-card class="card">
+    <div>
+      <el-button type="primary">测试</el-button>
+    </div>
+  </el-card>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
-<style scoped>
-
+<style lang="scss" scoped>
+.card {
+    height: 50vh;
+}
 </style>
